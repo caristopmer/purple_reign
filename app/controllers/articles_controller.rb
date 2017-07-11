ac5af215da4fd1bda49a79db0e76930ebeb9f251
Chris McMenomy <email": null>
2017-07-11 14:42:49 -0500

    new article action nearly complete.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,21 @@ get '/articles' do
   erb :"articles/index"
 end
 
+get '/articles/new' do
+  erb :"articles/show"
+end
+
+post '/articles' do
+  @article = Article.new(params[:article])
+  @article.author = current_user
+  if @article.save
+    redirect "/articles/#{@article.id}"
+  else
+    @errors = @article.errors.full_messages
+    erb :"users/new"
+  end
+end
+
 get '/articles/:id' do
   @article = Article.find(params[:id])
   erb :"articles/show"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ end
 
 post '/users' do
   @user = User.new(params[:user])
-  puts @user
   if @user.valid? && @user.password == params[:confirm_password]
     @user.save
     session[:user_id] = @user.id
